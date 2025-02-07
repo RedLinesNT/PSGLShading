db36301c7e92ae6bd680e8ab56fe45596d53f36d
RedLinesNT <77906555+RedLinesNT@users.noreply.github.com>
2025-02-07 11:23:24 +0100

    Fixed .gitignore issues

---
+++ src/Core/Debug/StatsConsole.cpp
@@ -0,0 +1,21 @@
+﻿#include "StatsConsole.h"
+
+StatsConsole* StatsConsole::Create() {
+    DebugConsoleInitOptions options;
+    options.Name = "StatsConsole";
+    options.PositionLeft = 0.0f;
+    options.PositionTop = 0.0f;
+    options.ConsoleWidth = 16;
+    options.ConsoleHeight = 2;
+    options.ConsoleScale = 1.0f;
+    options.Color = 0xFFFF64FF;
+    options.DoDebugPrints = false;
+    
+    return new StatsConsole(options);
+}
+
+StatsConsole::StatsConsole(const DebugConsoleInitOptions& options) : DebugConsole(options){}
+
+void StatsConsole::Update(float deltaTime) {
+    this->PrintfPut("DELTA TIME -> %e\n", deltaTime);
+}
---
+++ src/Core/Debug/StatsConsole.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Core/DebugConsole.h"
+
+class StatsConsole final : public DebugConsole {
+public:
+    StatsConsole(const DebugConsoleInitOptions& options);
+    virtual ~StatsConsole() = default;
+
+    static StatsConsole* Create();
+protected:
+    void Update(float deltaTime) override;
+};
